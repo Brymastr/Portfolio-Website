742f4ed4375a2babf759c1360332afb51f9c74d1
Brycen Dorsay <dorsay@live.ca>
2013-04-12 02:07:34 -0700

    JQuery animations for each "item"

--- Profile/float/JavaScript/actions.js
+++ Profile/float/JavaScript/actions.js
@@ -1,28 +1,29 @@
 $(document).ready(function() {
-   bounce(".item", 25, 15, 150, .8);
+	mouseOver(".item", 30, .8);
 
 });
 
-function bounce(id, over, out, time, multiplier) {
-	var bump_element = id;
+function mouseOver(id, time, multiplier) {
+	var bounce_element = id;
 	var timer = 0;
-	$(bump_element).each(function(i) {
+	$(bounce_element).mouseenter(function(i) {
+
 		
-		$(this).css("margin-left","-100px");
 		timer = (timer*multiplier + time);
-		$(this).animate({marginLeft: "0"}, timer);
-		$(this).animate({marginLeft: "15px"}, timer);
-		$(this).animate({marginLeft: "0"}, timer);
+		$(this).animate({top: "-10px"}, timer);
 	});
+	$(bounce_element).mouseleave(function(i) {
+
+		timer = (timer*multiplier + time);
+		$(this).animate({top: "0"}, timer);
+	});
+
 }
 
+//function mouseOver(id) {
+//document.getElementById(id).className = id + '_2';
+//}
 
-
-//JavaScript functions
-function mouseOver(id) {
-	document.getElementById(id).className = id + '_2';
-}
-
-function mouseOut(id) {
-	document.getElementById(id).className = id;
-}
+//function mouseOut(id) {
+//document.getElementById(id).className = id;
+//}
