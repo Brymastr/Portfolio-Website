f3e83217e00d33525588a8aa3207795ba0e1bbc6
Brycen Dorsay <dorsay@live.ca>
2014-05-09 22:02:56 -0700

    Restrict overflow of page content on slide-in/slide-out
    Fixed an issue where the page width would expand with the slide out animation of the page content. It was unnoticable on desktop browsers but in mobile browsers the page would zoom out quickly to fit in all of the page content.
    
    Added jquery.easing. Not sure if correctly implemented in actions.js

--- Profile/float/header.php
+++ Profile/float/header.php
@@ -10,6 +10,7 @@
 	<script src="js/holder.js"></script>
 	<script type="text/javascript" src="js/jquery.js"></script>
     <script type="text/javascript" src="js/jquery.animate-shadow.js"></script>
+    <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
 	<script type="text/javascript" src="js/actions.js"></script>
 </head>
 <body onunload="">
